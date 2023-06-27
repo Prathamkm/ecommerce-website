ba03fbbcd6ae17ee3dfb38f55f74eb4b64497084
Pratham kumar <prathamkumar6699@gmail.com>
2023-06-27 23:41:58 +0530

    project initialized

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const server = express();
+const mongoose = require("mongoose");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+}
+server.get("/", (req, res) => {
+  res.json({ status: "success" });
+});
+
+server.listen(8080, () => {
+  console.log("server started");
+});
