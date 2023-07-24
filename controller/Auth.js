fb3e6fd5140421bcdf9ef206de711d161ecada62
Pratham kumar <prathamkumar6699@gmail.com>
2023-07-24 05:49:02 +0530

    done with all authenticatons for user

--- controller/Auth.js
+++ controller/Auth.js
@@ -1,28 +1,42 @@
 const { User } = require("../model/User");
+const crypto = require("crypto");
+const { sanitizeUser } = require("../services/common");
+const SECRET_KEY = "SECRET_KEY";
+const jwt = require("jsonwebtoken");
 
 exports.createUser = async (req, res) => {
-  const user = new User(req.body);
   try {
-    const doc = await user.save();
-    res.status(201).json({ id: doc.id, role: doc.role });
-  } catch (error) {
-    res.status(400).json(error);
+    const salt = crypto.randomBytes(16);
+    crypto.pbkdf2(
+      req.body.password,
+      salt,
+      310000,
+      32,
+      "sha256",
+      async function (err, hashedPassword) {
+        const user = new User({ ...req.body, password: hashedPassword, salt });
+        const doc = await user.save();
+
+        req.login(sanitizeUser(doc), (err) => {
+          // this also calls serializer and adds to session
+          if (err) {
+            res.status(400).json(err);
+          } else {
+            const token = jwt.sign(sanitizeUser(doc), SECRET_KEY);
+            res.status(201).json(token);
+          }
+        });
+      }
+    );
+  } catch (err) {
+    res.status(400).json(err);
   }
 };
+
 exports.loginUser = async (req, res) => {
-  try {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) {
-      res.status(401).json({ message: "no such email" });
-    } else if (user.password === req.body.password) {
-      res.status(201).json({
-        id: user.id,
-        role: user.role,
-      });
-    } else {
-      res.status(401).json({ messaged: "invalid credentials" });
-    }
-  } catch (error) {
-    res.status(400).json(error);
-  }
+  res.json(req.user);
+};
+
+exports.checkUser = async (req, res) => {
+  res.json({ status: "success", user: req.user });
 };
